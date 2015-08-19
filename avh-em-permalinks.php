75efb3a31677d9f5f959497a39171084a2012cb0
Peter van der Does <github@avirtualhome.com>
2015-08-19 14:44:45 -0400

    Remove unwanted methods

--- avh-em-permalinks.php
+++ avh-em-permalinks.php
@@ -3,7 +3,7 @@
  * Plugin Name: AVH Event Manager Permalinks
  * Plugin URI:
  * Description: Create better permalink for Event Manager
- * Version: 0.1.0-dev.8
+ * Version: 0.1.0-dev.10
  * Author:      Peter van der Does
  * Author URI:
  * License:     GPLv2+
