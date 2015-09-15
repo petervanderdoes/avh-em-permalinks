c7fb123ccd2bf7d62417d3cc06bc5e0d4f184330
Peter van der Does <github@avirtualhome.com>
2015-09-15 16:48:11 -0400

    And we need to activate the plugin during the test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ require_once $_tests_dir . '/includes/functions.php';
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array('avh-em-permalinks/avh-em-permalinks.php'),
+    'active_plugins' => array('avh-em-permalinks/avh-em-permalinks.php','events-manager/events-manager.php')
 );
 
 require $_tests_dir . '/includes/bootstrap.php';
