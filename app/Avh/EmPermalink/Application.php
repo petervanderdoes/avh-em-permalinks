fa6911159f2630e4115221d14bb31d87478d6f79
Peter van der Does <github@avirtualhome.com>
2015-09-15 15:01:00 -0400

    Change directory for storing services file

--- app/Avh/EmPermalink/Application.php
+++ app/Avh/EmPermalink/Application.php
@@ -236,7 +236,7 @@ class Application extends Container implements ApplicationInterface
     public function registerConfiguredProviders()
     {
         $upload_dir_info = wp_upload_dir();
-        $manifestPath_directory = $upload_dir_info['basedir'] . '/avh-em-permalinks/framework';
+        $manifestPath_directory = $upload_dir_info['basedir'] . '/avh-em-permalinks/services';
         CommonHelper::createDirectory($manifestPath_directory);
         $manifestPath = $manifestPath_directory . '/services.json';
 
