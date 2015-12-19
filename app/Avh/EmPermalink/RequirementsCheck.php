1e6e691515ff4def0740cbb3586cb09beaa92530
Peter van der Does <github@avirtualhome.com>
2015-12-19 09:27:11 -0500

    Check for Dependencies based on named constant.

--- app/Avh/EmPermalink/RequirementsCheck.php
+++ app/Avh/EmPermalink/RequirementsCheck.php
@@ -11,9 +11,11 @@ namespace Avh\EmPermalink;
 class RequirementsCheck
 {
     private $file;
-    private $php = '5.2.4';
-    private $title = '';
-    private $wp = '3.8';
+    private $missing_dependencies = [];
+    private $pass_dependency      = false;
+    private $php                  = '5.2.4';
+    private $title                = '';
+    private $wp                   = '3.8';
 
     /**
      * Constructor
@@ -30,6 +32,29 @@ class RequirementsCheck
     }
 
     /**
+     * Check for dependency based on a named constant.
+     *
+     * @param string $define
+     * @param string $plugin
+     *
+     * @return bool
+     */
+    public function checkDependencyDefined($define, $plugin)
+    {
+        if (!defined($define)) {
+            add_action('admin_notices', [$this, 'displayDependencyNotice']);
+            add_action('admin_notices', [$this, 'deactivate']);
+            $this->removeActivatedNotice();
+            $this->missing_dependencies[] = $plugin;
+
+            return false;
+        }
+        $this->pass_dependency = true;
+
+        return true;
+    }
+
+    /**
      * Deactivate the plugin.
      */
     public function deactivate()
@@ -40,14 +65,32 @@ class RequirementsCheck
     }
 
     /**
+     * Display a notice when the plugin is missing de version is incompatible
+     */
+    public function displayDependencyNotice()
+    {
+        echo '<div class="error">';
+        echo '<p>The &#8220;' . esc_html($this->title) . '&#8221; plugin is missing depedencies.</p>';
+        if (count($this->missing_dependencies) > 1) {
+            $message = 'The following plugins are missing: ';
+        } else {
+            $message = 'The following plugin is missing: ';
+        }
+        echo '<p>' . $message . esc_html(implode(', ', $this->missing_dependencies)) . '</p>';
+        echo '</div>';
+    }
+
+    /**
      * Display a notice when the PHP version is incompatible
      */
     public function displayPhpVersionNotice()
     {
         echo '<div class="error">';
-        echo '<p>The &#8220;' . esc_html(
-                $this->title
-            ) . '&#8221; plugin cannot run on PHP versions older than ' . $this->php . '. Please contact your host and ask them to upgrade.</p>';
+        echo '<p>The &#8220;' .
+             esc_html($this->title) .
+             '&#8221; plugin cannot run on PHP versions older than ' .
+             $this->php .
+             '. Please contact your host and ask them to upgrade.</p>';
         echo '</div>';
     }
 
@@ -57,10 +100,20 @@ class RequirementsCheck
     public function displayWordPressVersionNotice()
     {
         echo '<div class="error">';
-        echo '<p>The &#8220;' . esc_html(
-                $this->title
-            ) . '&#8221; plugin cannot run on WordPress versions older than ' . $this->wp . '. Please update WordPress.</p>';
+        echo '<p>The &#8220;' .
+             esc_html($this->title) .
+             '&#8221; plugin cannot run on WordPress versions older than ' .
+             $this->wp .
+             '. Please update WordPress.</p>';
         echo '</div>';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasDependencies()
+    {
+        return $this->pass_dependency;
     }
 
     /**
@@ -133,6 +186,16 @@ class RequirementsCheck
             add_action('admin_notices', [$this, 'displayWordPressVersionNotice']);
 
             return false;
+        }
+    }
+
+    /**
+     * Removes the notice that a plugin is activated.
+     */
+    private function removeActivatedNotice()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
         }
     }
 }
